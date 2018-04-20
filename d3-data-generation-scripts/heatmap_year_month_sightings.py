1de7285f4b05ccf967739d16be65de3245758556
vrittirohira <vritti.rohira@gmail.com>
2018-04-19 21:17:11 -0700

    Fix in sighted_at column name

--- d3-data-generation-scripts/heatmap_year_month_sightings.py
+++ d3-data-generation-scripts/heatmap_year_month_sightings.py
@@ -1,10 +1,10 @@
 import pandas
 
 df = pandas.read_csv("../Data/ufo_awesome_v2.csv",compression='gzip')
-df['sighted_on']= pandas.to_datetime(df.sighted_on,errors='coerce')
-mask = (df['sighted_on'] > '2005-1-1') & (df['sighted_on'] <= '2016-1-1')
+df['sighted_at']= pandas.to_datetime(df.sighted_at,errors='coerce')
+mask = (df['sighted_at'] > '2005-1-1') & (df['sighted_at'] <= '2016-1-1')
 df2 =df.loc[mask]
-df2['month'] = df2.sighted_on.dt.month
-df2['year']= df2.sighted_on.dt.year
+df2['month'] = df2.sighted_at.dt.month
+df2['year']= df2.sighted_at.dt.year
 df3  = df2.groupby(['month','year'],as_index = False).country.agg({'sightings':'size'})
 df3.to_csv("../Data/heatmap_year_month_sightings.tsv", sep ='\t',index=False)
